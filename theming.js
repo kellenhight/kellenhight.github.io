ae54f97ea9365f1262f22a28d19cb884b2b5d820
Kellen Hight <kellenhight@ksu.edu>
2024-12-10 11:19:46 -0600

    Initial theme toggle support (probably broken)

--- theming.js
+++ theming.js
@@ -1,1 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var theme_toggle_button = document.getElementById("toggle_button");
 
+  function setTheme(theme) {
+    if(theme === "dark") {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.setAttribute('data-theme', 'dark');
+      theme_toggle_button.setAttribute('value', '<i class="bi-moon"></i>');
+    } else {
+      localStorage.setItem('theme', 'light');
+      document.documentElement.setAttribute('data-theme', 'light');
+      theme_toggle_button.setAttribute('value', '<i class="bi-sun-fill"></i>');
+    }
+  }
+  
+  function detectTheme() {
+    if(localStorage.getItem("theme")) {
+      return localStorage.getItem("theme");
+    } else if(!window.matchMedia) {
+      return false;
+    } else if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+
+    if (theme == "dark") {
+      document.documentElement.setAttribute("data-theme", "dark");
+      return "dark";
+    } else {
+      return "light";
+    }
+  }
+  setTheme(detectTheme());
+
+  theme_toggle_button.addEventListener("click", () => {
+    if(detectTheme() === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  });
+});
