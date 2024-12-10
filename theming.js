5c1f3a8eb7adca7b35075f9a0ebaf3bb2647f2ad
Kellen Hight <kellenhight@ksu.edu>
2024-12-10 11:27:34 -0600

    Fixed incorrect element selection in JS

--- theming.js
+++ theming.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var theme_toggle_button = document.getElementById("toggle_button");
+  var theme_toggle_button = document.getElementById("theme_button");
 
   function setTheme(theme) {
     if(theme === "dark") {
